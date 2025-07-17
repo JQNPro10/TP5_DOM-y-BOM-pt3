ad2102e8d65311acc117ead66b67a72afa8c65b0
Joaquin Albornoz Bresla <joacoalbornozbresla@gmail.com>
2025-07-17 15:45:58 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# ✨ Trabajo Práctico N° 5
+
+## 📓 Ejercicios de práctica de JS, prácticas con DOM y BOM
+
+En este repositorio vamos a resolver los ejercicio para practicar DOM y BOM
+# 🎇 Demo del proyecto.
+
+Para poder ver el proyecto, entrá 👉 [Aqui] https://tp5-pt3.netlify.app/
+
+# 💡Tecnologias utilizadas:
+
+- HTML5
+- JavaScript
+- CSS
+- [BOOTSTRAP](https://getbootstrap.com/docs/5.3/getting-started/introduction/)
+
+# 📢 Autor
+
+- [Joaquin Albornoz Bresla](https://github.com/jqnpro10)
---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar una tarea y un botón que al ser presionado agregue dicha tarea a una lista, cada elemento ingresado en la lista debe poder ser eliminado con un botón creado para ese fin. 
+*/
+
+const input = document.getElementById("input");
+const btnAgregar = document.getElementById("btnAgregar");
+const tareas = document.getElementById("tareas");
+
+btnAgregar.addEventListener("click", () => {
+    const tarea = input.value.trim();
+    if (tarea) {
+        const elementoLista = document.createElement("li");
+        elementoLista.className = "list-group-item d-flex justify-content-between align-items-center px-2 border";
+        elementoLista.style.marginBottom = "0.5rem";
+
+        const spanTarea = document.createElement("span");
+        spanTarea.textContent = tarea;
+
+        const eliminarBoton = document.createElement("button");
+        eliminarBoton.className = "btn btn-danger ms-3";
+        eliminarBoton.textContent = "Eliminar";
+        eliminarBoton.addEventListener("click", () => {
+            tareas.removeChild(elementoLista);
+        });
+
+        elementoLista.appendChild(spanTarea);
+        elementoLista.appendChild(eliminarBoton);
+        tareas.appendChild(elementoLista);
+        input.value = "";
+    } else {
+        alert("Por favor, ingrese una tarea.");
+    }
+});
